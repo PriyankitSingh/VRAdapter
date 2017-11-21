9c60ca4836ac02f01ca5a4f09761c527b3cffa78
PriyankitSingh <psin778@aucklanduni.ac.nz>
2017-11-22 12:53:55 +1300

    Added initial files (doesn't work)

---
+++ AdapterScripts/SMIPlatform.cs
@@ -0,0 +1,52 @@
+﻿using SMI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMIPlatform : GazeMonoBehaviour
+{
+    private VRAdapter adapter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnGazeEnter(RaycastHit hitInformation)
+    {
+        base.OnGazeEnter(hitInformation);
+        Debug.Log("on enter smi");
+        if (adapter != null)
+        {
+            adapter.OnPointerEnter();
+        }
+    }
+
+    public override void OnGazeExit()
+    {
+        base.OnGazeExit();
+        if (adapter != null)
+        {
+            adapter.OnPointerExit();
+        }
+    }
+
+    public override void OnGazeStay(RaycastHit hitInformation)
+    {
+        base.OnGazeStay(hitInformation);
+        if (adapter != null)
+        {
+            adapter.OnPointerStay(hitInformation);
+        }
+    }
+
+    public void setAdapter(VRAdapter adapter)
+    {
+        this.adapter = adapter;
+    }
+}
---
+++ AdapterScripts/VRAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SMI;
+
+/**
+ * Defines the type of platform for which the script is being written.
+ * Generic means non eye tracking. Its uses the middle of the screen
+ * as a selector.
+ * */
+public enum Platform
+{
+    FOVE, SMIVive, Generic
+}
+public class VRAdapter : MonoBehaviour {
+    public Platform targetPlatform;
+	// Use this for initialization
+	void Start () {
+        // get the type of main camera
+        // this is used to check which type of platform we have
+        
+        gameObject.AddComponent<SMIPlatform>();
+        SMIPlatform smi = gameObject.GetComponent<SMIPlatform>();
+
+        smi.setAdapter(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+
+    public virtual void OnPointerEnter() {
+        Debug.Log("on enter adapter");
+    }
+
+    public virtual void OnPointerExit() { }
+
+    public virtual void OnPointerStay(RaycastHit hitInformation) { }
+}
